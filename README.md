a7a5389773a2a4fc6e16634288365685bc18fbad
jpeterson1823 <petersonjohn418@gmail.com>
2024-05-01 11:41:47 -0500

    Cleaning up main branch

--- README.md
+++
@@ -1,3 +0,0 @@
-This branch is for uploading all PyQt code for the Raspberry Pi.
-
-I will be trying to code in Python whenever possible instead of C++
--- sensor_module_base/include/hardware/serial_interface.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-void serial_recv(std::string& buf);
-void serial_send(const char* str);
-void serial_send(std::string msg);
-void serial_ack();
-void serial_clear();
